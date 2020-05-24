7742420288f202f9c472b2758e00a9ee7d081e27
Kat M <musiukkasia@gmail.com>
2020-05-24 22:22:07 +0100

    changing path in the deployment file

--- TerraformAppServiceWithLocalGit/TerraformAppService/deploymentFile.sh
+++ TerraformAppServiceWithLocalGit/TerraformAppService/deploymentFile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../HelloWorldDotNetCore
+cd ../../HelloWorldDotNetCore
 #log in as terraform service principal
 az login --service-principal -u $APP_URL -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 url=`az webapp deployment list-publishing-credentials --name "terraform-appservice-kmusiuk" --resource-group "terraform-test-group" --query scmUri --output tsv`
